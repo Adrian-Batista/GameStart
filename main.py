a899fb11d465a387c04b58bffff2e059ebdb0183
Adrian Augusto Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-10-28 21:10:28 -0300

    Logica Finalizada

--- main.py
+++ main.py
@@ -2,19 +2,33 @@ import random
 
 opcao = "SIM"
 
-username = input('Digite seu nome: ')
-print(f"Seja Bem Vindo(a), {username}.!")
+class Pessoa:
+    def __init__(self, nome, pontuacao, tentativas):
+        self.nome = nome
+        self.pontuacao = tentativas
+        self.tentativas = tentativas
+
+user1 = Pessoa(str(input('Digite seu nome: ')), 10, 3)
+valor = random.randint(1,10)
 
 while opcao == "SIM":
-    valor = random.randint(1,10)
-
-    pontuacao = 10
-    tentativas = 3
-
-    
+    print(f"Seja Bem Vindo(a), {user1.nome}.!")
     print("------------------------------------------ \n")
-    print(f" - Selecione um número de 1 a 10, você tem {tentativas} tentativas: ")
+    print(f" - Selecione um número de 1 a 10, você tem {user1.tentativas} tentativas: ")
     aux = False
+
+    def reinicio(aux = ''):
+        aux = str(input("\n - Se desejar começar novamente digite ( SIM ), se deseja sair pressione ENTER : "))
+        aux = aux.upper()
+        user1.pontuacao = 10
+        user1.tentativas = 3
+        valor = random.randint(1,10)
+        return aux
+
+    def impressao():
+        print("\n ----------------------------------------- \n\n")
+        print(f" - Usuário: {user1.nome}\n - Pontuação até o momento: {user1.pontuacao}\n - tentativas : {user1.tentativas}")
+        print("\n\n ----------------------------------------- \n")
 
     while (aux == False):
         escolha = input(" - Digite: ")
@@ -31,13 +45,28 @@ while opcao == "SIM":
             print("O Valor digitado não é um número, por favor repita a operação!")
             aux = False
 
-    print(f"- O Valor escolhido foi {escolha}, a resposta é {valor}.!")
+    #print(f"- O Valor escolhido foi {escolha}, a resposta é {valor}.!\n")
+    
+    if(escolha == valor):
+        print(" - Parabéns você acerto!")
+        impressao()
+        opcao = reinicio(opcao)
+
+    elif(user1.tentativas == 3):
+        user1.pontuacao = 5
+        user1.tentativas -=1
+        print(f" - Que pena, sua resposta foi incorreta, {user1.nome} você tem {user1.tentativas} tentativas restantes!")        
+
+    elif(user1.tentativas == 2):
+        user1.pontuacao = 2
+        user1.tentativas -=1
+        print(f" - Que pena, sua resposta foi incorreta, {user1.nome} você tem {user1.tentativas} tentativas restantes!")
+
+    else:
+        user1.pontuacao = 0
+        print(" - Que pena, suas  tentativas esgotaram, reinicialize o game!")
+        impressao()
+        opcao = reinicio(opcao)
         
-
-
-
-
-    opcao = str(input("Se desejar começar novamente digite ( SIM ), se deseja sair pressione ENTER : "))
-    opcao = opcao.upper()
 else:
     print("Volte Sempre!!")
